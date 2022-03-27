45aecbb2d4de01494f0c6d9c5d5bd3a132b9f302
Chepuri Sunil <chepurisunil3@gmail.com>
2022-03-27 21:15:25 +0530

    Syntax error in test files

--- tests/add.test.js
+++ tests/add.test.js
@@ -5,20 +5,26 @@ describe('Tests for Sum of given numbers', () => {
     expect(sumOfGivenNumbers("")).toBe(0) // if the string is empty return sum as 0
   })
   test('Check for NAN', () => {
-    expect(sumOfGivenNumbers("8;abc")).toThrow('Invalid inputs given - abc')
+    expect(() => {
+      sumOfGivenNumbers("8;abc")
+    }).toThrow('Invalid inputs given - abc')
   })
   test('Check for single number', () => {
-    expect(sumOfGivenNumbers("2")).toBe(2) // for the single number input, return the same value as sum
+    expect(sumOfGivenNumbers("2")).toBe("2") // for the single number input, return the same value as sum
   })
 
   test('Check for multiple numbers', () => {
     expect(sumOfGivenNumbers("2;3")).toBe(5) // return sum of given numbers
-    expect(sumOfGivenNumbers("5;10")).toBe(15) // return sum of given numbers
+    expect(sumOfGivenNumbers("//<>\n5<>10")).toBe(15) // return sum of given numbers
   })
   test('Check for -ve numbers', () => {
-    expect(sumOfGivenNumbers("-4;-8")).toThrow('negatives not allowed - -4,-8')
+    expect(() => {
+      sumOfGivenNumbers("-4;-8")
+    }).toThrow('negatives not allowed - -4,-8')
   })
   test('Check for invalid delimiter pattern', () => {
-    expect(sumOfGivenNumbers("-4:-8")).toThrow('Values given with Invalid delimiter pattern. Please check the description for correct pattern')
+    expect(() => {
+      sumOfGivenNumbers("//<>-4<>-8")
+    }).toThrow('Values given with Invalid delimiter pattern. Please check the description for correct pattern')
   })
 })
